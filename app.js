a0eff7d4e28242beda3d3422ddec1ce19f46baf8
AlexaVas <alexandra.vasinova@gmail.com>
2023-09-06 17:46:39 +0200

    rename route

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+app.use("/", indexRoutes);
 
 const mailRoutes = require("./routes/mail.routes");
 app.use("/", mailRoutes);
